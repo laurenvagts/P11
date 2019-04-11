1f63eb5fa6f8f9f569446a039e13b9449640a342
Pete Fennell <42587875+pfennel-golden@users.noreply.github.com>
2019-04-11 13:40:09 -0600

    Update P11.java

--- P11.java
+++ P11.java
@@ -4,6 +4,7 @@ public class P11 {
 
    public static void main(String[] args) throws Exception {
 
+      Scanner keys = new Scanner( System.in );
       String name;
 
       if ( args.length == 1 ) {
@@ -11,7 +12,6 @@ public class P11 {
       }
       else {
          System.out.print("Enter name of P11 program file: ");
-         Scanner keys = new Scanner( System.in );
          name = keys.nextLine();
       }
 
@@ -19,13 +19,21 @@ public class P11 {
       Parser parser = new Parser( lex );
 
       // start with <statements>
-      Node root = parser.parseDefs();
+      Node libraryExpressions = parser.parseDefs();
 
       // display parse tree for debugging/testing:
       TreeViewer viewer = new TreeViewer("Parse Tree", 0, 0, 800, 500, root );
 
-      // execute the parse tree
-      // root.execute();
+      while (true) {
+        String input = keys.nextLine();
+          String FileNamePath = "expression.txt";
+	       PrintWriter printWriter=new PrintWriter(bw);
+          bw.write( input);
+          lex = new Lexer( "expression.txt" );
+          parser = new Parser( lex );
+          Node root = parser.parseExpr();
+          root.execute(libraryExpressions);
+      }
 
    }// main
 
